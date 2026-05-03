a6dba0245f8bb6dcb254e291e9f0facfb57e8c04
akashchandra-code <akashchandra6280@gmail.com>
2026-05-03 22:16:45 +0530

    add render link

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://crypto-ai-gq4m.onrender.com",
     credentials: true,
     methods: [ "GET", "POST", "PUT", "DELETE" ],
 }))
